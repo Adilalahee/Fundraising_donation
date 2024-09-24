e260cc08473e1818959b41a2d1af7926727bd6b9
Adil Alahee <adilmdctg@gmail.com>
2024-09-24 09:22:05 -0700

    History function added

--- Js/style.js
+++ Js/style.js
@@ -6,11 +6,22 @@ document.getElementById('donate_btn').addEventListener('click',function(){
     if(donateAmount<currentBalance){
         const newbalance=currentBalance-donateAmount;
     document.getElementById('current_balance').innerText=newbalance;
+    const div=document.createElement('div');
+    div.classList.add('my-2');
+    div.classList.add('border-2');
+    div.classList.add('border-solid');
+    div.classList.add('p-4');
+    div.innerHTML=`
+    <h4> ${donateAmount} Taka has been donated </h4>
+    <p> Date: ${new Date().toString()} </p>
+    `
+    
+    document.getElementById('history_transaction').appendChild(div);
     }
     else{
         alert('Sorry')
     }
     const totalBalance=addmoneyBalance+donateAmount;
-    document.getElementById('addmoney_balance').innerHTML=totalBalance;   
+    document.getElementById('addmoney_balance').innerText=totalBalance;   
 })
 
